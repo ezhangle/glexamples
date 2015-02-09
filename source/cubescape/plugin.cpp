bec8a49c06ce3b926b366b32717cc06c23a55378
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2015-02-09 14:41:26 +0100

    add viewer and cubescape example from gloperate

---
+++ source/cubescape/plugin.cpp
@@ -0,0 +1,15 @@
+#include <gloperate/plugin/plugin_api.h>
+
+#include "CubeScape.h"
+
+#include <glexamples-version.h>
+
+GLOPERATE_PLUGIN_LIBRARY
+
+    GLOPERATE_PLUGIN(CubeScape
+    , "CubeScape"
+    , "Renders a landscape built of cubes"
+    , GLEXAMPLES_AUTHOR_ORGANIZATION
+    , "v1.0.0" )
+
+GLOPERATE_PLUGIN_LIBRARY_END
